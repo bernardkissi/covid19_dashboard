0ea86c0c7d3d94ed6101e0c65f4aacd8418ecd93
bernardk23 <bernardkissi@live.com>
2020-05-16 13:27:51 +0000

    Input theme modifier added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <div
+    class="text-gray-700 dark:text-gray-200 max-w-7xl rounded-md mt-4 lg:py-10 py-6 px-4 sm:px-8 lg:px-8 mx-auto"
+  >
+    <div class="grid grid-cols-1 gap-0 lg:grid-cols-4 lg:gap-12">
+      <div class="col-span-1 lg:col-span-2">
+        <div class="relative">
+          <input
+            v-model="search"
+            class="p-4 px-16 bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200 rounded-md w-full shadow-xl 
+            dark:placeholder-gray-300 placeholder-gray-700"
+            type="text"
+            placeholder="Search Regions, eg. Greater Accra, Ashanti, Northen"
+          />
+          <div class="absolute top-0">
+            <svg class="h-6 w-6 mt-4 ml-6" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                stroke="#4A5568"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-1 lg:col-span-2">
+        fsdsfdfds
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  data: () => ({})
+  data: () => ({
+    search: ''
+  })
 }
 </script>
 
