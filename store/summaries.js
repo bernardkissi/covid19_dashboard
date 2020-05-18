81f167dad3f07efda8f20c817f7c3334104d6ed8
bernardk23 <bernardkissi@live.com>
2020-05-18 09:18:53 +0000

    Source Modified

--- store/summaries.js
+++ store/summaries.js
@@ -17,7 +17,7 @@ export const mutations = {
 export const actions = {
   async fetchWorld({ commit }) {
     const response = await this.$axios.get(
-      'https://2d983fe7.ngrok.io/api/world'
+      'https://d85ed9c7.ngrok.io/api/world'
     )
     commit('putWorld', {
       time: response.data.updated,
--- store/trends.js
+++ store/trends.js
@@ -36,8 +36,8 @@ export const mutations = {
 export const actions = {
   async fetchTrends({ commit }) {
     const [daily, weekly] = await Promise.all([
-      this.$axios.get('https://2d983fe7.ngrok.io/api/getDailyUpdates'),
-      this.$axios.get('https://2d983fe7.ngrok.io/api/getWeeklyUpdates')
+      this.$axios.get('https://d85ed9c7.ngrok.io/api/getDailyUpdates'),
+      this.$axios.get('https://d85ed9c7.ngrok.io/api/getWeeklyUpdates')
     ])
     commit('putDaily', daily.data)
     commit('putWeekly', weekly.data)
