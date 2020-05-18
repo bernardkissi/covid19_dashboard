534146683c151c2d09b7f4801181cc2830540bf9
bernardk23 <bernardkissi@live.com>
2020-05-18 04:03:44 +0000

    purge config modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@
  */
 module.exports = {
   // purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  },
   theme: {
     darkSelector: '.dark-mode',
     container: {
