cb8f3eec8a1269b2cde54ac3c70fd2ef6c690a18
bernardk23 <bernardkissi@live.com>
2020-05-19 15:56:20 +0000

    Localstorage js modified

--- plugins/localstorage.js
+++ plugins/localstorage.js
@@ -1,15 +1,9 @@
 // plugins/vuex-persistedstate.js
 import createPersistedState from 'vuex-persistedstate'
 
-const NAME_SPACE = 'vuex'
-
 export default ({ store, isHMR, isClient }) => {
   if (isHMR) return
-  if (isClient) {
-    createPersistedState({
-      key: NAME_SPACE,
-      paths: ['regions', 'trends', 'summaries']
-      // ...
-    })(store)
-  }
+  window.onNuxtReady((nuxt) => {
+    createPersistedState()(store)
+  })
 }
