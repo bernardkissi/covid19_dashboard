2522d9aeb3334ef4548ceffc4e039cf826de0c03
bernardk23 <bernardkissi@live.com>
2020-05-24 06:55:34 +0000

    modified websocket configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,12 +68,12 @@ export default {
       '@nuxtjs/laravel-echo',
       {
         broadcaster: 'pusher',
-        key: 'dmB12C35',
-        cluster: 'mt1',
+        key: process.env.PUSHER_APP_KEY,
+        cluster: process.env.PUSHER_APP_CLUSTER,
         encrypted: true,
         wsHost: '127.0.0.1',
-        wsPort: 6001,
-        wssPort: 6001,
+        wsPort: process.env.PUSHER_PORT,
+        wssPort: process.env.PUSHER_PORT,
         disableStats: true,
         enabledTransports: ['ws', 'wss']
       }
